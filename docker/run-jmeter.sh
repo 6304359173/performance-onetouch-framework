eac22858151a105e7c4f8f01b58e10f2f0748313
Ramesh Babu <sigharam01@gmail.com>
2026-07-13 14:42:21 +0530

    Add Dockerfile and run-jmeter script

---
+++ docker/run-jmeter.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Cleaning old reports..."
+
+rm -rf /tests/reports/*
+rm -f /tests/results/results.jtl
+
+echo "Running JMeter..."
+
+jmeter -n \
+-t /tests/jmeter/testplans/RestfulBooker.jmx \
+-q /tests/config/DEV.properties \
+-l /tests/results/results.jtl \
+-e \
+-o /tests/reports
+
+echo "JMeter execution completed."
